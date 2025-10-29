4015dbdb70b3869534934fcde564dd499d30cbfe
Will Reed <wreed@disroot.org>
2025-10-29 11:44:02 -0500

    chore: add type declaration namespace

--- src/god/types.cpp
+++ src/god/types.cpp
@@ -1,7 +1,33 @@
+#include <unordered_map>
 #include <string>
+#include <variant>
+#include <cstdint>
+#include <vector>
 
 namespace god {
-
+    namespace type {
+        
+        struct map;
+        struct list;
     
+        using value = std::variant<
+            std::string,
+            std::int64_t,
+            double,
+            bool,
+            std::nullptr_t,
+            map,
+            list
+        >;
+    
+        struct map {
+            std::unordered_map<std::string, value> members;
+        };
+    
+        struct list {
+            std::vector<value> elements;
+        };
+    
+    }
 
 }
