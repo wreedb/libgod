a913a65f6ecb18cab18adbdef6a283fdbc1053f4
Will Reed <wreed@disroot.org>
2025-10-29 11:45:05 -0500

    refactor: use fully-qualified function/type names in input.cpp

--- src/god/input.cpp
+++ src/god/input.cpp
@@ -9,29 +9,23 @@
 
 namespace fs = std::filesystem;
 
-using std::optional;
-using std::string;
-using std::cerr;
-using std::vector;
-using std::println;
-
 namespace god {
     namespace input {
-        optional<vector<string>> file(const string& path) {
+        std::optional<std::vector<std::string>> file(const std::string& path) {
             if (!fs::exists(fs::path(path))) {
-                println(cerr, "file '{}' doesn't exist", path);
+                std::println(std::cerr, "file '{}' doesn't exist", path);
                 return std::nullopt;
             }
             
             std::ifstream handle(path);
             
             if (!handle.is_open()) {
-                println(cerr, "error opening file '{}'", path);
+                std::println(std::cerr, "error opening file '{}'", path);
                 return std::nullopt;
             }
             
-            vector<string> linesVec;
-            string line;
+            std::vector<std::string> linesVec;
+            std::string line;
             while (std::getline(handle, line)) {
                 linesVec.push_back(line);
             }
@@ -39,9 +33,9 @@ namespace god {
             return linesVec;
         }
         
-        vector<string> stream(std::istream& in) {
-            vector<string> linesVec;
-            string line;
+        std::vector<std::string> stream(std::istream& in) {
+            std::vector<std::string> linesVec;
+            std::string line;
             while (std::getline(in, line)) {
                 linesVec.push_back(line);
             }
