9a260d00dd1fd02ffd9b2d8d77e81b24bedf9867
Will Reed <wreed@disroot.org>
2026-02-26 22:24:41 -0600

    chore: added double min/max tests

---
+++ test/double-min.test.cpp
@@ -0,0 +1,8 @@
+#include <god.hpp>
+
+auto main(int argc, const char **argv) -> int {
+    // this will attempt to parse a double 1 lesser than the minimum limit of the specification
+    auto number = god::parse::number(god::token{god::tokentype::number, "-9223372036854775809.0", 0, 0, 0, 0});
+    if (not number) return 0;
+    else return 1;
+}
