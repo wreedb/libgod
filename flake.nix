ce87251decd51506012b8fe7ae4c8daea34da364
Will Reed <wreed@disroot.org>
2026-02-26 21:45:18 -0600

    chore: add no-clobber test

---
+++ flake.nix
@@ -0,0 +1,58 @@
+{
+    description = "Official reference implementation of the God data language";
+
+    inputs = {
+        nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+        flake-utils.url = "github:numtide/flake-utils";
+    };
+
+    outputs = { self, nixpkgs, flake-utils }:
+        flake-utils.lib.eachDefaultSystem (system:
+            let
+                pkgs = nixpkgs.legacyPackages.${system};
+                lib  = nixpkgs.legacyPackages.${system}.lib;
+                stdenv = pkgs.llvmPackages_21.stdenv;
+                version = builtins.readFile ./.version;
+
+                nativeBuildInputs = with pkgs; [
+                    meson
+                    ninja
+                ];
+
+                libgod-package = stdenv.mkDerivations {
+                    pname = "libgod";
+                    inherit version;
+                    src = ./.;
+
+                    meta = {
+                        description = "Official reference implementation of the God data language";
+                        long-description = ''
+                            C++ parsing library for the God data serialization format. The library is
+                            exception-free while retaining highly robust error reporting information,
+                            an intuitive API as well as convenient tools like outputting directly to
+                            JSON.
+                        '';
+                        homepage = "https://wreedb.github.io/god";
+                        license = lib.licenses.lgpl3Plus;
+                    };
+
+                    inherit nativeBuildInputs;
+
+                    mesonBuildType = "release";
+                    mesonFlags = [
+                        "-D libcxx=true"
+                        "-D strip=true"
+                    ];
+                };
+
+            in {
+                packages.libgod = libgod-package;
+                packages.default = libgod-package;
+
+                devShells.default = pkgs.mkShell {
+                    inherit nativeBuildInputs;
+                };
+            }
+        );
+        
+}
---
+++ test/no-clobber.test.cpp
@@ -0,0 +1,28 @@
+#include <god.hpp>
+#include <cassert>
+
+auto main(int argc, const char **argv) -> int {
+    
+    std::string path{argv[1]};
+
+    auto input = god::input::file(path);
+
+    if (not input) return 1;
+
+    auto scanner = god::scanner(&input.value());
+    auto tokens = scanner.scan();
+
+    if (not tokens) tokens.error().panic();
+
+    auto document = god::document{};
+    
+    auto field_one = god::field{.name = "test", .val = god::map{}};
+    auto field_two = god::field{.name = "test", .val = god::list{}};
+    auto result_one = document.add(field_one);
+    auto result_two = document.add(field_two);
+    
+    assert(result_one.value() == 0);
+    assert(result_two.error() == god::error{"a field with identifier 'test' is already present"});
+    
+    return 0;
+}
