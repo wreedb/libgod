db2a91cb2bdd5efcf28dec5836b29f216f448d4f
Will Reed <wreed@disroot.org>
2026-02-27 17:10:28 -0600

    chore: tests for usage of null/true/false as identifiers

---
+++ test/null-identifier.test.cpp
@@ -0,0 +1,32 @@
+#include <god.hpp>
+#include <cassert>
+
+auto main(int argc, const char **argv) -> int {
+    
+    std::string path{argv[1]};
+
+    auto input = god::input::file(path);
+
+    if (not input) return 1;
+
+    auto scanner = god::scanner(&input.value());
+    auto tokens = scanner.scan();
+
+    if (not tokens) tokens.error().panic();
+
+    auto document = god::parse::document(*tokens);
+
+    if (not document) document.error().panic();
+
+    auto doc = document.value();
+
+    auto test = doc.fields.at(0);
+
+    auto control = god::field{
+        .name = "null",
+        .val = god::value{true}
+    };
+
+    assert(control == test);
+    return 0;
+}
